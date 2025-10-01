15fac134803d60c2ad640734d4429a9da60b3e88
MuNash <nash.collage.life@gmail.com>
2025-10-01 13:31:04 +0700

    Tugas 3

---
+++ flowchartTugas2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class flowchartTugas2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int angka;
+
+        System.out.print("Masukkan Angka: ");
+        angka = input.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Bilangan Genap");
+        }else{
+            System.out.println("Bilangan Ganjil");
+        }
+    }
+}
