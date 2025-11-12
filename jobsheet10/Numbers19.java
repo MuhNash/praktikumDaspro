fed2be781286b643cd95f9ed15fa0c283ae0ba76
MuhNash <nash.collage.life@gmail.com>
2025-11-12 13:00:28 +0700

    Percobaan 3

---
+++ jobsheet10/Numbers19.java
@@ -0,0 +1,8 @@
+public class Numbers19 {
+    public static void main(String[] args) {
+        int[][] myNumbers = new int[3][1];
+        myNumbers[0] = new int[5];
+        myNumbers[1] = new int[2];
+        myNumbers[2] = new int[1];
+    }
+}
