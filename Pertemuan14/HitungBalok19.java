5cc6d99469f5b7c788a36a324dee383606dc1d51
MuhNash <nash.collage.life@gmail.com>
2025-11-26 14:05:02 +0700

    Percobaan 5

---
+++ Pertemuan14/HitungBalok19.java
@@ -0,0 +1,33 @@
+package Pertemuan14;
+import java.util.Scanner;
+
+public class HitungBalok19 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int p, l, t, L, vol;
+
+        System.out.print("Masukkan panjang: ");
+        p = input.nextInt();
+        System.out.print("Masukkan lebar: ");
+        l = input.nextInt();
+        System.out.print("Masukkan tinggi: ");
+        t = input.nextInt();
+
+        L = hitungLuas(p, l);
+        System.out.println("Luas Persegi Panjang adalah: " + L);
+        
+        vol = hitungVolume(p, l, t);
+        System.out.println("Volume Balok adalah: " + vol);
+
+        input.close();
+    }
+
+    static int hitungLuas(int pjg, int lb){
+        int Luas = pjg * lb;
+        return Luas;
+    }
+    static int hitungVolume(int tinggi, int a, int b){
+        int volume = hitungLuas(a, b) * tinggi;
+        return volume;
+    }
+}
