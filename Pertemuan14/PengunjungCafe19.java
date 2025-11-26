f8998fba023cb0d571746d01039e2ea7627e3ef6
MuhNash <nash.collage.life@gmail.com>
2025-11-26 13:58:21 +0700

    Percobaan 4

---
+++ Pertemuan14/PengunjungCafe19.java
@@ -0,0 +1,16 @@
+package Pertemuan14;
+
+public class PengunjungCafe19 {
+    public static void main(String[] args) {
+        daftarPengunjung("Ali", "Budi", "Citra");
+        daftarPengunjung("Andi");
+        daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+    }
+
+    static void daftarPengunjung(String...namaPengunjung){
+        System.out.println("Daftar Nama Pengunjung: ");
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
+        }
+    }
+}
